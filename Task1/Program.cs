9f6c6fea2b1810023cc3861cbcd5224446ff89d4
AstonDV <astondv.kms@gmail.com>
2023-02-11 03:58:27 +1000

    End task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+
+string PrintNumber(int n)
+{
+    if(n == 1) return "1";
+    else
+    {
+        return PrintNumber(n - 1) + " "+n.ToString();
+    }
+}
+
+Console.WriteLine(PrintNumber(8));
