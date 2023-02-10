49b421f5b6a61cafe1e33b5c2aa434df2ca9d687
AstonDV <astondv.kms@gmail.com>
2023-02-11 04:02:04 +1000

    End task 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+
+string PrintNumber(int m, int n)
+{
+    if (n == m) return n.ToString();
+    else
+    {
+        return PrintNumber(m, n - 1) + " " + n.ToString();
+    }
+}
+
+Console.WriteLine(PrintNumber(4, 8));
