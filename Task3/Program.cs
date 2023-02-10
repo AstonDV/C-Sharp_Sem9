d18f351ebfd61c618fbaef01e4d7838d34d1c22c
AstonDV <astondv.kms@gmail.com>
2023-02-11 04:10:43 +1000

    End task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+int GetSumOfDigits(int num)
+{
+    if (num == 0) return 0;
+    else return num % 10 + GetSumOfDigits(num / 10);
+}
+
+Console.WriteLine(GetSumOfDigits(453));
