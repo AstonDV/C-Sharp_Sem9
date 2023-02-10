61273a4078e05e982798be7808326bafa345c1ea
AstonDV <astondv.kms@gmail.com>
2023-02-11 04:17:11 +1000

    End task 4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа A и B, 
+// и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+
+void Main()
+{
+    Console.WriteLine("Введите число А: ");
+    int numberA = Convert.ToInt32(Console.ReadLine()!);
+    Console.WriteLine("Введите число В: ");
+    int numberB = Convert.ToInt32(Console.ReadLine()!);
+
+    Console.WriteLine(Pow(numberA, numberB));
+}
+
+int Pow(int a, int b)
+{
+    if (b == 0) return 1;
+    else
+    {
+        return a * Pow(a, b - 1);
+    }
+}
+
+Main();
